ac4ccef6bdfa79c789cea1e9fe1b2f3d6398cd2e
Brytton Tsai <btsai8@my.bcit.ca>
2023-11-17 16:11:39 -0800

    updated run.py file to print results

--- run.py
+++ run.py
@@ -3,10 +3,13 @@ import time
 import argparse
 import platform
 import subprocess
+from numpy import column_stack
 import pandas as pd
 import datetime
 from constants import IMPORT_PACKAGE
 
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
 # Dynamic Import for loaded model
 try:
     file = open(IMPORT_PACKAGE, "r")
@@ -17,30 +20,22 @@ except Exception as e:
     print(f"Error failed to import model package: {e}")
     exit(1)
 
+
 def check_folder(folder_path):
     if not os.path.exists(folder_path):
         raise FileNotFoundError(f"The folder '{folder_path}' does not exist.")
+
 
 def get_new_csv_files(folder_path, last_seen_files):
     all_files = set(os.listdir(folder_path))
     new_files = all_files - last_seen_files
     return new_files
 
-def process_new_csv_files(folder_path, new_files):
-    for file in new_files:
-        if file.endswith('.pcap'):
-            pcap_file_path = os.path.join(folder_path, file)
-            csv_file_path = os.path.join(folder_path, file.replace(".pcap", ".csv"))
-            convert_pcap_to_csv(pcap_file_path, csv_file_path)
-            print(f"New CSV file '{csv_file_path}' has been created:")
-            df = pd.read_csv(csv_file_path)
-            result = model.predict(df)
-            print(result)
-            print("\n")
 
 def convert_pcap_to_csv(pcap_file_path, csv_file_path):
     command = f"cicflowmeter -f {pcap_file_path} -c {csv_file_path}"
-    subprocess.run(command, shell=True)
+    subprocess.run(command, shell=True, stdout=subprocess.DEVNULL)
+
 
 def predict_function(file_path):
     # print(f"Running test function with argument: {test_arg}")
@@ -48,10 +43,12 @@ def predict_function(file_path):
     result = model.predict(df)
     print(result)
 
+
 def test_function():
     print(f"Model loaded successfully, this means build was successful!")
 
-def monitor_folder(folder_path):
+
+def monitor_folder(folder_path, delay=5):
     last_seen_files = set()
     print(f"Started Monitoring {folder_path}")
     while True:
@@ -62,17 +59,17 @@ def monitor_folder(folder_path):
         else:
             print("No New Files")
 
-        time.sleep(5)
+        time.sleep(delay)
 
-def check_tcp_dump(folder_path):
+
+def check_tcp_dump():
     if not check_command_available("tcpdump"):
-        print("Missing TCP Dump Command, please insatll with (apt-get install tcpdump)")
+        print("Missing tcpdump Command, please insatll with (apt-get install tcpdump)")
         exit(1)
     else:
-        print("TCP Dump command found")
+        print("tcpdump command found")
 
         
-
 def check_command_available(command):
     try:
         # Use 'where' on Windows, 'which' on Unix-like systems
@@ -83,6 +80,30 @@ def check_command_available(command):
         return True
     except subprocess.CalledProcessError:
         return False
+
+
+def process_new_csv_files(folder_path, new_files):
+    """
+    Edit this function to change prediction reuslt behaviour
+    """
+    for file in new_files:
+        if file.endswith('.pcap'):
+            pcap_file_path = os.path.join(folder_path, file)
+            csv_file_path = os.path.join(folder_path, file.replace(".pcap", ".csv"))
+            convert_pcap_to_csv(pcap_file_path, csv_file_path)
+            print(f"New CSV file '{csv_file_path}' has been created:")
+
+            df = pd.read_csv(csv_file_path)
+            result_df = model.predict(df)
+
+            columns_to_print = ["timestamp", "protocol", "src_ip", "src_port", "src_mac", "dst_ip", "dst_port", "dst_mac", "attack_type"]
+
+            result_df = result_df[columns_to_print]
+
+            suspicious_df = result_df[result_df["attack_type"] != "BENIGN"]
+            if len(suspicious_df) != 0:
+                print(suspicious_df)
+                print("\n")
 
 
 def main():
@@ -119,30 +140,16 @@ def main():
     network_interface = args.interface
 
     try:
-        check_tcp_dump(folder_path)
+        check_tcp_dump()
         # Define the tcpdump command
         # tcpdump_command = f"sudo tcpdump -i {network_interface} -n -w {os.path.join(folder_path, 'capture_$(date +%Y%m%d%H%M%S).pcap')} -G 5"
         out_file = os.path.join(folder_path, "outfile-%s.pcap")
-        tcpdump_command = f"sudo tcpdump -i {network_interface} -w {out_file} -G 10 -n"
+        tcpdump_command = f"sudo tcpdump -i {network_interface} -w {out_file} -G 15 -n"
 
         # tcpdump_process = subprocess.Popen(tcpdump_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-        tcpdump_process = subprocess.Popen(tcpdump_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        # subprocess.run(tcpdump_command, shell=True)
-
-        # # Wait for a certain period (you can customize this)
-        # time.sleep(10)
-        #
-        # # Terminate the tcpdump process
-        # tcpdump_process.terminate()
-        #
-        # # Optionally, wait for the process to finish and get the return code
-        # return_code = tcpdump_process.wait()
-        # print(f"tcpdump process terminated with return code: {return_code}")
-
         print(f"tcpdump process started successfully")
-
-        monitor_folder(folder_path)
+        time.sleep(5)
+        monitor_folder(folder_path, delay=5)
 
     except KeyboardInterrupt:
         print("Monitoring stopped.")
